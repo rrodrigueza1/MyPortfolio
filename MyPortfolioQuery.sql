6b4df19f9e9dbe03fb642ba46371ccfa1d98efa7
Renzelle Frank Rodrigueza <rrodrigueza1@nait.ca>
2018-05-08 02:03:37 -0600

    Add Database Connection to Azure SQL Server

---
+++ MyPortfolioQuery.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE Project (
+    ProjectId int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    ProjectName varchar(50) NOT NULL,
+    ProjectDescription varchar(255) NOT NULL,
+	DateCreated date NOT NULL
+);
+
+CREATE TABLE Image (
+    ImageId int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    ImageName varchar(50) NOT NULL,
+    ImageDescription varchar(255) NOT NULL,
+	Image image NOT NULL,
+	ProjectId int NOT NULL,
+	CONSTRAINT FK_ProjectImage FOREIGN KEY (ProjectId)
+    REFERENCES Project(ProjectId)
+);
